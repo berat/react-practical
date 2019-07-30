52894e49a545bb5e1426402ae27c58045128acbf
Berat Bozkurt <beratbozkurt1999@gmail.com>
2019-07-31 00:07:55 +0300

    boostrap install

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+
 import Header from './component/header';
 import Content from './component/content/';
 import Footer from './component/footer/';
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Content />
       <Footer />
