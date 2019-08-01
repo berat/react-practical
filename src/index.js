a9f094de2669c253ccd178dacff0ea80265927f2
Berat Bozkurt <beratbozkurt1999@gmail.com>
2019-08-01 16:18:59 +0300

    css to sass

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './main.css';
+import './main.sass';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
