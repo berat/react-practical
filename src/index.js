c706d24ba6425c2b0fe411bc78ff97dad8f3a764
Berat Bozkurt <beratbozkurt1999@gmail.com>
2019-08-02 00:28:50 +0300

    customize coding

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './main.sass';
+import './main.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
