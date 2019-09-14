4d232119e9feee32de99684f564d12d18d675e60
Berat Bozkurt <beratbozkurt1999@gmail.com>
2019-09-14 21:10:36 +0300

    edited

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, BrowserRouter as Router, Route } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 const Header = () => {
@@ -8,39 +8,41 @@ const Header = () => {
 	const [aktiflik, setAktiflik] = useState(false);
 
 	useEffect(() => {
-		
 
-        if(Cookies.get("login")){
+
+		if (Cookies.get("login")) {
 			setAktiflik(true)
 		}
-		
+
 	}, [])
 
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<div className="container">
-				<NavLink className="navbar-brand" to="/">react-practical</NavLink>
-				<button className="navbar-toggler" onClick={() => setToggleIsVisible(!toggleIsVisible)} type="button">
-					<span className="navbar-toggler-icon" />
-				</button>
-				<div style={{ display: toggleIsVisible ? "none" : "block" }} className="justify-content-end navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav justify-content-end">
-						<li className="nav-item">
-							<NavLink activeClassName="active" className="nav-link" to="/">Ansayfa</NavLink>
-						</li>
-						<li className="nav-item" style={{ display : aktiflik===false ? "none": "inherit" }}>
-							<NavLink activeClassName="active" className="nav-link" to="/profil">Profil</NavLink>
-						</li>
-						<li className="nav-item" style={{ display : aktiflik===true ? "none": "inherit" }}>
-							<NavLink activeClassName="active" className="nav-link" to="/giris-yap">Giriş Yap</NavLink>
-						</li>
-						<li className="nav-item" style={{ display : aktiflik===true ? "none": "inherit" }}>
-							<NavLink activeClassName="active" className="nav-link" to="/kayit-ol">Kayıt Ol</NavLink>
-						</li>
-					</ul>
+		<Router>
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<div className="container">
+					<NavLink className="navbar-brand" to="/">react-practical</NavLink>
+					<button className="navbar-toggler" onClick={() => setToggleIsVisible(!toggleIsVisible)} type="button">
+						<span className="navbar-toggler-icon" />
+					</button>
+					<div style={{ display: toggleIsVisible ? "none" : "block" }} className="justify-content-end navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav justify-content-end">
+							<li className="nav-item">
+								<NavLink activeClassName="active" className="nav-link" to="/">Ansayfa</NavLink>
+							</li>
+							<li className="nav-item" style={{ display: aktiflik === false ? "none" : "inherit" }}>
+								<NavLink activeClassName="active" className="nav-link" to="/profil">Profil</NavLink>
+							</li>
+							<li className="nav-item" style={{ display: aktiflik === true ? "none" : "inherit" }}>
+								<NavLink activeClassName="active" className="nav-link" to="/giris-yap">Giriş Yap</NavLink>
+							</li>
+							<li className="nav-item" style={{ display: aktiflik === true ? "none" : "inherit" }}>
+								<NavLink activeClassName="active" className="nav-link" to="/kayit-ol">Kayıt Ol</NavLink>
+							</li>
+						</ul>
+					</div>
 				</div>
-			</div>
-		</nav>
+			</nav>
+		</Router>
 	)
 }
 
